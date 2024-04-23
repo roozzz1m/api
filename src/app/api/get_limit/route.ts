8885fa759b2a4440b9bfd658b6663df34acd574f
рутя <109959967+roozzz1m@users.noreply.github.com>
2024-04-23 16:20:01 +0500

    Add files via upload

---
+++ src/app/api/get_limit/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse, NextRequest } from "next/server";
+import { corsHeaders } from "@/lib/utils";
+export const dynamic = "force-dynamic";
+import { newSunoApi } from '@/lib/SunoApi'
+
+export async function GET(req: NextRequest) {
+  if (req.method === 'GET') {
+    try {
+      const suno = await newSunoApi()
+      const limit = await suno.get_credits();
+
+      return new NextResponse(JSON.stringify(limit), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching limit:', error);
+
+      return new NextResponse(JSON.stringify({ error: 'Internal server error. ' + error }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    }
+  } else {
+    return new NextResponse('Method Not Allowed', {
+      headers: {
+        Allow: 'GET',
+        ...corsHeaders
+      },
+      status: 405
+    });
+  }
+}
+
+export async function OPTIONS(request: Request) {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders
+  });
+}
